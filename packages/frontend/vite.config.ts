bb54d582505ee50743d95cc3a0b0c58e26a28df4
ductlv <aws.tutorial.vn@gmail.com>
2023-05-21 19:32:30 +0700

    fix: build fe

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicDir: '../../public',
+  publicDir: '../backend/public',
   plugins: [
     rollupReplace({
       preventAssignment: true,

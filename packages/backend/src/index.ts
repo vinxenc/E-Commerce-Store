f79c68f3541a50f9cbcfb6b8a52727639a5ede7d
ductlv <aws.tutorial.vn@gmail.com>
2023-05-21 20:37:16 +0700

    fix: build fe

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -40,6 +40,8 @@ app.use(cors(corsOptions));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ limit: '1mb', extended: true }));
 
+console.log(path.join(root.path, 'public'));
+console.log(path.join(__dirname, 'public'));
 app.use(express.static(path.join(root.path, 'public')));
 
 // app.get('/', (_: express.Request, res: express.Response) => {
